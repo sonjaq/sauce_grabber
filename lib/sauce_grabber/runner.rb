854c8a62d3a9c0ae005deecde5734ca51535bfca
Andie Leaf <avleaf@gmail.com>
2015-11-22 18:09:23 -0600

    Magic sauce

--- lib/sauce_grabber/runner.rb
+++ lib/sauce_grabber/runner.rb
@@ -2,39 +2,82 @@ require 'sauce_grabber'
 
 module SauceGrabber
   class Runner
-    attr_accessor   :options, :browsers, :urls, :current_browser
+    attr_accessor   :options, :browsers, :urls, :current_browser, 
+                    :errors, :failed, :success
     
     def initialize
       opts = Slop.parse do |o|
-        # require "pry";binding.pry # temporary debug statment
-        o.string '-b', '--browsers-file', 'browser csv file' 
-        o.string '-u', '--urls-file', 'csv file of urls' 
-        o.bool '-v', '--verbose', 'enable verbose mode'
+        o.string '-b', '--browsers-file', 'browser csv file (default: browsers.csv)', default: "browsers.csv"
+        o.string '-u', '--urls-file', 'csv file of urls (default: urls.csv)', default: "urls.csv"
+        o.string '-o', '--output-dir', 'Output directory', default: "screenshots"
         o.on '--version', 'print the version' do
-          puts SauceGrabber::VERSION
+          puts "SauceGrabber #{SauceGrabber::VERSION}"
           exit
         end
+        o.on '-h', '--help','Display this help' do
+         puts o
+         exit
+        end
       end
+
+      @errors  = Array.new
+      @failed  = Array.new
+      @success = Array.new
+
       @options  = opts
-      @browsers = capabilities_array(opts[:browsers_file])
-      @urls     = urls_array(opts[:urls_file])
+      @browsers = capabilities_array(opts[:browsers_file] )
+      @urls     = urls_array(opts[:urls_file] )
       go
     end
 
     # Consider adding utility fields with chunk processing here
     def capabilities_array(browser_csv)
-      capabilities = SmarterCSV.process(browser_csv)
-      capabilities.map do |caps|
-        CapabiltiesFactory.new(caps)
+      unless File.exists?(browser_csv)
+        puts "Please provide a browser csv file and try again."
+        finish
       end
+      capabilities = SmarterCSV.process(browser_csv,:convert_values_to_numeric => false)
+      capabilities.map do |caps|
+        SauceGrabber::CapabilitiesFactory.new(caps)
+      end
+      capabilities
     end
 
     def urls_array(urls_csv)
-      SmarterCSV.process(urls_csv)
+      unless File.exists?(urls_csv)
+        puts "Please provide a URLs csv file and try again."
+        finish
+      end
+      SmarterCSV.process(urls_csv) 
     end
 
     def go
-      puts @options
+      Swurl.output_dir = options[:output_dir]
+      browse_capabilities
+    rescue => e
+      errors.push(e)
+    ensure
+      finish
+    end
+
+    def browse(caps)
+      browser = Browser.new(caps)
+      success.push(Swurl.swurl(browser, urls))
+    rescue => e
+      failed.push(caps)
+      errors.push({ error: e, caps: caps })
+    end
+
+    def browse_capabilities
+      @browsers.each_with_index do |caps, i|
+        browse(caps)
+      end
+    end
+
+    def finish
+      puts "Screenshots are available in #{Swurl.output_dir}",
+           "Thanks for using Sauce Grabber."
+      # Exit
       exit
     end
 
--- lib/sauce_grabber/swurl.rb
+++ lib/sauce_grabber/swurl.rb
@@ -1,26 +1,80 @@
 module SauceGrabber
   class Swurl
+    class << self
 
-    attr_accessor   :browser, :url, :anchors, :template
+      def swurl(browser, urls)
+        @current_browser = browser
+        @browser = browser.start_browser
+        @urls = urls
 
-    def initialize(url, template = nil)
-      @url = url
-      @template = template
-      @browser = Watir::Browser.new() # Grab this from a current browser list
+        urls.each do |u|
+          @browser.goto(u[:url])
+          shots = get_screenshots(@browser)
+          shots.each_with_index { |s, i| 
+            next if s == nil
+            File.open(url_to_path(u,i), 'wb') { |f| f.write(s)} 
+          }  
+        end
+
+      rescue => e
+        # quiet in case this fails
+      ensure
+        @browser.quit
+        return { browser: browser, urls: urls }
+      end
+
+      def url_to_path(url,index)
+        target_dir = screenshot_dir
+        ensure_screenshot_directory(target_dir)
+        filename = prepare_filename(url, index)
+        File.join(target_dir, filename)
+      end
+
+      def screenshot_dir
+        File.join(
+            output_dir,
+            @current_browser.os,
+            @current_browser.name,
+            @current_browser.version
+          )
+      end
+
+      def ensure_screenshot_directory(dir)
+        unless File.exists?(dir)
+          FileUtils.mkdir_p(dir)
+        end
+      end
+
+      def prepare_filename(url,index)
+        f = "#{url[:template]}-#{safe_filename(url[:url])}"
+        if index > 0 
+          f +=  "-#{index}"
+        end
+        f + ".png"
+      end
+
+      def output_dir=(arg)
+        @output_dir = arg
+      end
+
+      def output_dir
+        @output_dir
+      end
+
+      def safe_filename(url)
+        path = URI.parse(url).path
+        return "root" if path == ""
+        path.gsub(/(\/|\.)/, "-").chomp("-")
+      end
+
+      def get_screenshots(browser)
+        # Returns [] of screenshots
+        SauceGrabber::Shooter.shoot(browser)
+      rescue => e
+        require "pry";binding.pry # temporary debug statment
+        return []
+      end
+
     end
-
-    def view
-      browser.goto(url)
-    rescue e
-      require "pry";binding.pry # temporary debug statment
-    end
-
-    def shot
-      Shooter.screenshot(browser)
-      Shooter.footer_screenshot(browser)
-    rescue e
-      require "pry";binding.pry # temporary debug statment
-    end
-
   end
 end
