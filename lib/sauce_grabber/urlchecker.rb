7e8e4d3c134982aa017a047a90841853abbc2333
tmulry <tmulry@gmail.com>
2017-09-29 14:46:38 -0500

    Add broken image check on each page as error output

---
+++ lib/sauce_grabber/urlchecker.rb
@@ -0,0 +1,32 @@
+module SauceGrabber
+  class UrlChecker
+    class << self
+
+      def check(browser)
+        @browser = browser
+        shots = []
+        begin
+            shots = checkurls
+
+        rescue Selenium::WebDriver::Error::UnsupportedOperationError => e
+          # This gets past an issue with JS unsupported errors
+        end
+        return shots
+      end
+
+      def checkurls()
+        script = %q{var list, images = [], index;
+      list = document.querySelectorAll('.entry-article img');
+      for (index = 0; index < list.length; ++index) {
+          if (list[index].naturalWidth == "undefined" || list[index].naturalWidth == 0) {
+            images.push(list[index]);
+          }
+      }
+      return images}
+        @browser.execute_script(script)
+      end
+
+    end
+
+  end
+end
